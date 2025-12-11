10edcbe849edeb54f0399672637991dde80181f0
indextrown <indextrown@gmail.com>
2025-12-11 16:28:29 +0900

    chore: Swagger 순서 설정

--- src/main/java/com/serverbaseapi/be/domain/auth/presentation/AuthController.java
+++ src/main/java/com/serverbaseapi/be/domain/auth/presentation/AuthController.java
@@ -6,6 +6,7 @@ import com.serverbaseapi.be.domain.auth.dto.response.SignupResponseDto;
 import com.serverbaseapi.be.domain.auth.google.application.GoogleAuthService;
 import com.serverbaseapi.be.domain.auth.google.dto.request.GoogleAppLoginRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,7 +35,8 @@ public class AuthController {
     @PostMapping("/google/mobile/login")
     @Operation(summary = "[App] 구글 로그인"
             ,description = "구글 로그인 버튼을 눌러 발급받은 id_token을 사용해 로그인합니다.(서버 로그인 아닙니다.)",
-            tags = {"[Auth] 구글"}
+            tags = {"[Auth] 구글"},
+            operationId = "1_googleLogin"
     )
     public ResponseEntity<LoginResponseDto> googleMobileLogin(@RequestBody GoogleAppLoginRequestDto googleAppLoginRequestDto) {
         LoginResponseDto loginResponseDto = googleAuthService.mobileLogin(googleAppLoginRequestDto);
@@ -44,7 +46,8 @@ public class AuthController {
     // ----- 구글 회원가입 -----
     @Operation(summary = "[App] 구글 + 데이터베이스 회원가입"
             ,description = "구글 앱에서 발급받은 id_token을 사용해 로그인합니다.",
-            tags = {"[Auth] 구글"}
+            tags = {"[Auth] 구글"},
+            operationId = "2_googleLogin"
     )
     @PostMapping("/google/signup")
     public ResponseEntity<SignupResponseDto> googleSignup(@RequestBody SignupRequestDto signupRequestDto) {
